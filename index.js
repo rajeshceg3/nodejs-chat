60d2b160e5dc4c93b7366ed5c31c906eae62f351
rajeshceg3 <rajeshceg3@gmail.com>
2022-12-09 11:57:06 +0530

    2 files modified, 2 added

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-// run `node index.js` in the terminal
+const express = require('express')
+const socketio = require('socket.io')
+const PORT = 3000
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const app = express()
+const server = app.listen(PORT, ()=>{
+  console.log(`Server listening on ${PORT}`)
+})
+
+const io = socketio(server)
+io.on('connection', ( socket)=>{
+
+  // Received a messaged from connected client
+  socket.on('message',(message)=>{
+    console.log(`Message received from client is ${message}`)
+  })
+
+  // Send a broadcast to others
+  socket.broadcast.emit('message', message)
+
+  socket.on('disconnect',()=>{
+    console.log("Client disconnected")
+  })
+})
