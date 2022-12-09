40c9d714e20759b784cbc87cc37e722874073486
rajeshceg3 <rajeshceg3@gmail.com>
2022-12-09 12:28:55 +0530

    Added support for authentication with passport

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 const express = require('express')
 const socketio = require('socket.io')
+const passport = require('passport')
+const session = require('express-session')
 const PORT = 3000
 
 const app = express()
+app.use(session({ secret : 'secret-key'}))
+app.use(passport.initialize())
+app.use(passport.session())
+
 const server = app.listen(PORT, ()=>{
   console.log(`Server listening on ${PORT}`)
 })
 
 const io = socketio(server)
 io.on('connection', ( socket)=>{
+  passport.authenticate('jwt',{ session: false}, ( err, user)=>{
+    if ( err || !user ){
+      socket.emit('error', "User not authenticated");
+      return;
+    }
 
+    console.log(`${user.username} is authenticated and logged in`);
+    
   // Received a messaged from connected client
   socket.on('message',(message)=>{
-    console.log(`Message received from client is ${message}`)
+    console.log(`Message received from ${user.username} is ${message}`)
   })
 
   // Send a broadcast to others
-  socket.broadcast.emit('message', message)
+  socket.broadcast.emit('message', {
+    user : user.username,
+    message: message
+  })
 
   socket.on('disconnect',()=>{
-    console.log("Client disconnected")
+    console.log(`${user.username} is now disconnected`);
+  })
   })
 })
