6f98a93019b691a46a2aecae3c0b90826345de30
rajeshceg3 <rajeshceg3@gmail.com>
2022-12-09 16:42:26 +0530

    Added Bcrypt support for hashing

--- index.js
+++ index.js
@@ -2,18 +2,43 @@ const express = require('express')
 const socketio = require('socket.io')
 const passport = require('passport')
 const session = require('express-session')
+const bcrypt = require('bcryptjs')
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
 const PORT = 3000
 
 const app = express()
+app.use(bodyParser.urlencoded({extended:false}))
 app.use(session({ secret : 'secret-key'}))
 app.use(passport.initialize())
 app.use(passport.session())
 
+// Set up a temporary store for user data
+// Later we'll hook it up to db
+const users = new Map();
+const jwtSecret = 'jwt-secret-code';
+
+// Initialize the server
 const server = app.listen(PORT, ()=>{
   console.log(`Server listening on ${PORT}`)
 })
 
-const io = socketio(server)
+// Collect username and password and validate it
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>Super Fast chat </h1>
+    <form action="/login" method="post">
+      <input type="text" name="username" placeholder="Username" required />
+      <input type="password" name="password" placeholder="Password" required />
+      <button type="submit">Login</button>
+    </form>
+  `);
+});
+
+// Initialize socket io
+const io = socketio(server);
+
+
 io.on('connection', ( socket)=>{
   passport.authenticate('jwt',{ session: false}, ( err, user)=>{
     if ( err || !user ){
@@ -39,3 +64,61 @@ io.on('connection', ( socket)=>{
   })
   })
 })
+
+app.post('/register',(req,res)=>{
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if(users.has(username)){
+    res.status(409).send("Provided username is not available");
+    return;
+  }
+
+  bcrypt.hash(password, 10, ( err, hash)=>{
+    if( err ){
+      res.status(500).send("Issue with hashing the password");
+      return;
+    }
+
+    users.set(username, {
+      username: username,
+      password: hash
+    });
+
+    res.send("User Registration is Successful");
+
+  })
+})
+
+function getUserDetails(username){
+  return users.get(username);
+}
+
+app.post('login',(req,res)=>{
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if ( !users.has(username)){
+    res.status(401).send(`User ${username} is not registered`);
+    return;
+  }
+
+  const userdetails = getUserDetails(username);
+  const passwordValid = bcrypt.compareSync(password, userdetails.password);
+
+  if(!passwordValid){
+    res.json({
+      success: false,
+      message : " Incorrect credentials"
+    })
+  }
+  else{
+    const token = jwt.sign({username:userdetails.username}, 'Secret-key');
+    res.json({
+      success: true,
+      token : token
+    })
+  }
+
+  })
